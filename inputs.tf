77ec504ed552b57930b724cbf125c875f8058f9a
Josh Sinfield <josh.sinfield@cdl.co.uk>
2019-03-12 17:43:56 +0000

    Initial commit with resources to create a hosted vif and accept the vif

---
+++ inputs.tf
@@ -0,0 +1,25 @@
+variable "address_family" {
+  default = "ipv4"
+}
+
+variable "vlan_prefix" {
+  default = 1
+}
+
+variable "aws_region" {
+  default = "eu-west-1"
+}
+
+variable "bgp_asn" {}
+
+variable "amazonAddress" {}
+
+variable "name" {}
+
+variable "vlan_id" {}
+
+variable "connection_id" {}
+
+variable "owner_account_id" {}
+
+variable "vgw_id" {}
---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_dx_hosted_private_virtual_interface" "private_vif" {
+  provider         = "aws.source"
+  address_family   = "${var.address_family}"
+  bgp_asn          = "${var.bgp_asn}"
+  connection_id    = "${var.connection_id}"
+  name             = "${var.name}"
+  owner_account_id = "${var.owner_account_id}"
+  vlan             = "${var.vlan_id}"
+  amazon_address   = "${var.amazonAddress}"
+}
+
+resource "aws_dx_hosted_private_virtual_interface_accepter" "private_vif_accepter" {
+  provider             = "aws.owner"
+  virtual_interface_id = "${aws_dx_hosted_private_virtual_interface.private_vif.id}"
+  vpn_gateway_id       = "${var.vgw_id}"
+}
