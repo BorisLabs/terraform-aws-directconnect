17847ee4e17959f180d0bf0f86957cbfadcf3ef2
Ben Arundel <ben.arundel@cdl.co.uk>
2020-03-10 10:31:12 +0000

    updating the outputs to be latest tf compatible and adding descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,46 @@
 output "dx_connection_id" {
-  value = aws_dx_connection.this[0].id
+  value = concat(aws_dx_connection.this.*.id, [""])[0]
+  description = "The ID of the connection"
 }
 
 output "dx_connection_bandwidth" {
-  value = aws_dx_connection.this[0].bandwidth
+  value = concat(aws_dx_connection.this.*.bandwidth, [""])[0]
+  description = "Bandwidth of the connection"
 }
 
 output "dx_connection_arn" {
-  value = aws_dx_connection.this[0].arn
+  value = concat(aws_dx_connection.this.*.arn, [""])[0]
+  description = "The ARN of the connection"
 }
 
 output "dx_connection_name" {
-  value = aws_dx_connection.this[0].name
+  value = concat(aws_dx_connection.this.*.name, [""])[0]
+  description = "The connection name"
 }
 
 output "dx_private_hosted_vif_id" {
-  value = aws_dx_hosted_private_virtual_interface.private_vif[0].id
+  value = concat(aws_dx_hosted_private_virtual_interface.private_vif.*.id, [""])[0]
+  description = "The ID of the virtual interface."
 }
 
 output "dx_private_hosted_vif_arn" {
-  value = aws_dx_hosted_private_virtual_interface.private_vif[0].arn
+  value = concat(aws_dx_hosted_private_virtual_interface.private_vif.*.arn, [""])[0]
+  description = "The ARN of the virtual interface"
 }
 
 output "dx_gateway_id" {
-  value = aws_dx_gateway.this[0].id
+  value = concat(aws_dx_gateway.this.*.id, [""])[0]
+  description = "The ID of the gateway"
 }
 
 output "dx_gateway_name" {
-  value = aws_dx_gateway.this[0].name
+  value = concat(aws_dx_gateway.this.*.name, [""])[0]
+  description = "The name of the gateway"
 }
 
 output "vgw_id" {
-  value = aws_vpn_gateway.this[0].id
+  value = concat(aws_vpn_gateway.this.*.id, [""])[0]
+  description = "The ID of the VPN Gateway"
 }
+
+
